30b8ea6cadcea64075af8a6301104b3b59baa543
DFreel1st <crystler93@gmail.com>
2023-02-09 22:55:15 +0300

    Examples with using aspects, part 2.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,17 +5,23 @@ import model.Comment;
 import services.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-    var service = context.getBean(CommentService.class);
+        var service = context.getBean(CommentService.class);
 
         Comment comment = new Comment();
         comment.setAuthor("Natasha");
         comment.setText("Demo comment");
 
-        service.publishComment(comment);
+        String  value = service.publishComment(comment);
+
+        logger.info(value);
     }
 }
